e1642811db3ae14105253e805b7a954252b9b940
Bones <heaugo@gmail.com>
2023-10-08 22:04:56 -0400

    plot rush de fin

--- MoonQuakePy/app.py
+++ MoonQuakePy/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify
-from obspy import read
+from obspy import read, read_inventory, UTCDateTime
+import numpy as np
+import matplotlib.pyplot as plt
 import os
+
+import csv
 
 app = Flask(__name__)
 
@@ -14,44 +18,88 @@ def seismic_data():
     # # Read seismic data using ObsPy (replace with your data file)
     # Obtenez le chemin absolu du fichier en utilisant os.path.join
     shz = "ressources/mseed/xa.s16..shz.1972.112.0.mseed"
+    mhz = "ressources/mseed/xa.s16.00.mhz.1972.112.0.mseed"
     mh1 = "ressources/mseed/xa.s16.00.mh1.1972.112.0.mseed"
-    print(mh1)
-    # Ouvrez le fichier en utilisant le chemin absolu
+    mh2 = "ressources/mseed/xa.s16.00.mh2.1972.112.0.mseed"
+    att = "ressources/mseed/xa.s16..att.1972.112.0.mseed"
+    
     st = read(mh1)
+    st += read(shz)
+    st += read(mhz)
+    st += read(mh2)
+    st += read(att)
+
+    #st.plot('plot.png')
+    st.sort(['starttime'])
+    st.normalize()
+    t0 = st[0].stats.starttime
+
+    fig, axes = plt.subplots(nrows=len(st)+1, sharex=True)
+    ax = None
+
+    st.plot(outfile='sorted_plot.png')
+    
+    for (tr, ax) in zip(st, axes):
+        ax.plot(tr.times(reftime=t0), tr.data)
+
+    st.merge(method=1)
+    axes[-1].plot(st[0].times(reftime=t0), st[0].data, 'r')
+    axes[-1].set_xlabel(f'seconds relative to {t0}')
+    plt.show()
+    plt.savefig("matplotlib")
+
     # Process the data and extract necessary information
     # For example, you can return the number of traces in the stream
     print(st)
     
     num_traces = len(st)
-    
-    tr = st[0]
-    print(tr)
-    print(tr.stats)
-    print(tr.data)
-    st.plot(outfile='mh1.png')
-    st.plot(outfile='mh1dayplot.png', type='dayplot')
-    st.plot(outfile='mh1section.png', type='section')
-    shz = "ressources/mseed/xa.s16..shz.1972.112.0.mseed"
-    st = read(shz)
-    st.plot(outfile='shz.png')
-    
-    shz = "ressources/mseed/xa.s16.00.mhz.1977.112.0.mseed"
-    st = read(shz)
-    st.plot(outfile='mhz.png')
-    
-    shz = "ressources/mseed/xa.s16.00.mh2.1977.112.0.mseed"
-    st = read(shz)
-    st.plot(outfile='mh2.png')
+   
+    st.filter('highpass', freq=0.5, corners=2, zerophase=True)
+    st.plot(outfile='plot_filter.png')
 
-    shz = "ressources/mseed/xa.s16..att.1977.112.0.mseed"
-    st = read(shz)
-    st.plot(outfile='att.png')
+
     # Return the data as JSON
-    data = {
-        'num_traces': num_traces,
-        'sample_rate': st[0].stats.sampling_rate if num_traces > 0 else None
-    }
-    return jsonify(data)
+    for t in st:
+        print(t.data)
+
+    
+    return jsonify("OK")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+    #for t in st:
+    #     t = tr.times()
+    #     data = tr.data
+    #     i = 0
+    #     for d in data:
+    #         data[i] = d * 0.08
+    #         i+=1
+    #     i = 0
+    #     for d in data:
+    #         data[i] = round(d, 2)
+    #         i+=1
+    #     data = list(set(data))
+    #     plt.plot(t, data)
+    # plt.savefig('plot_matplotlib_filter.png')
+
+    # ##plt.ylabel()
+
+
+    # csv_name = "du.csv"
+    # print(data[0])
+    # i = 0
+    # for d in data:
+    #     data[i] = round(d, 2)
+    #     i+=1
+
+
+    # with open(csv_name, 'w') as csv_file:
+    #     writer = csv.writer(csv_file, delimiter=' ') 
+    #     # Write the data rows
+    #     for row in list(set(data)):
+    #         print(row)
+    #         r = []
+    #         r.append(row)
+    #         writer.writerow(r)
